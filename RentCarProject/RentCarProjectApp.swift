5d4aa21cfd18383ecb69b6ebc0cce30c62be0ca4
RetroJet <rretrojet@gmail.com>
2025-10-30 23:23:29 +0100

    Initial Commit

---
+++ RentCarProject/RentCarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentCarProjectApp.swift
+//  RentCarProject
+//
+//  Created by Nazar on 30.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct RentCarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
